f12283b1e23ebf3ddc869c346f3d987f7c7bd4b9
A <A>
2016-05-04 11:09:23 +0100

    4-5-2016

---
+++ Task 3/blog.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1510996
+ * Date: 04/05/2016
+ * Time: 11:07
+ */
--- Task 3/index.php
+++ Task 3/index.php
@@ -1,3 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Index</title>
+</head>
+<body>
+<div id="wrap">
+    <header>
+        <div id="section">
+            <h1>myBlog</h1>
+            <p id="subtitle">because the internet needs to know what I think</p>
+        </div>
+    </header>
+    <div id="mainbody">
+        <div id="navigation">
+            <ul>
+                <li><a href="blog.html">All Blog Items</a></li>
+                <li><a href="blog.html">Work Items</a></li>
+                <li><a href="blog.html">University Items</a></li>
+                <li><a href="blog.html">Family Items</a></li>
+                <li><a href="add.html">Insert a Blog Item</a></li>
+            </ul>
+        </div>
+        <div id="maincontent">
+            <div id="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <img src="logo.png">
+        </div>
+    </div>
+    <footer>Designed by Chaniotis Vasileios-Efthymios, 2016</footer>
+</div>
+</body>
+</html>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +42,5 @@
  * Date: 04/05/2016
  * Time: 11:06
  */
+
+?>

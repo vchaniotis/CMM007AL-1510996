f0f6dc1c2969e4cb8a2912f65d9749444ce53aab
A <A>
2016-05-04 11:10:53 +0100

    4-5-2016

--- Task 3/index.php
+++ Task 3/index.php
@@ -16,11 +16,11 @@
     <div id="mainbody">
         <div id="navigation">
             <ul>
-                <li><a href="blog.html">All Blog Items</a></li>
-                <li><a href="blog.html">Work Items</a></li>
-                <li><a href="blog.html">University Items</a></li>
-                <li><a href="blog.html">Family Items</a></li>
-                <li><a href="add.html">Insert a Blog Item</a></li>
+                <li><a href="blog.php">All Blog Items</a></li>
+                <li><a href="blog.php">Work Items</a></li>
+                <li><a href="blog.php">University Items</a></li>
+                <li><a href="blog.php">Family Items</a></li>
+                <li><a href="add.php">Insert a Blog Item</a></li>
             </ul>
         </div>
         <div id="maincontent">
